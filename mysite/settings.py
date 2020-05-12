f0bfb23fae2b75f0aa8826f96d53240ca72270cf
blacwh <blacwh@hotmail.com>
2020-05-12 19:08:36 +0900

    try settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,3 +140,6 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# correct the static file problem
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
